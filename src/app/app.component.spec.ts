84616f68081e888d5d7f36c602fb45c7a5996f03
Vladyslav Shchetyna <vshchetyna@lenovo.com>
2020-08-29 12:04:55 +0300

    Added routing and general styles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'BitWallet'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('BitWallet');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('BitWallet app is running!');
-  });
 });
