7f5ba523a9322e7f7522f23e23ab71c218630f30
Vladyslav Shchetyna <vshchetyna@lenovo.com>
2020-09-05 13:32:33 +0300

    [0.9] Dashboard markup. Added icons

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { CryptoList } from '../../interfaces/CryptoList';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  cryptoList: CryptoList[];
+  selectedCrypto: string;
+  @ViewChild('img', {static: false}) image;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.cryptoList = [
+      {name: 'Bitcoin', ticker: 'BTC', imageUrl: '../../../assets/icons/svg/icon-Bitcoin.svg'}, 
+      {name: 'Ether', ticker: 'ETH', imageUrl: '../../../assets/icons/svg/icon-Ethereum.svg'},
+      {name: 'Ripple', ticker: 'XRP', imageUrl: '../../../assets/icons/svg/icon-Xrp.svg'}
+    ];
   }
 
+  ngAfterViewInit() {
+    this.image.nativeElement.src = this.cryptoList[0].imageUrl;
+  }
+
+  selectChangeHandler($event: any): void {
+    this.selectedCrypto = $event.target.value;
+    this.changeCryptoImage(this.selectedCrypto);
+  }
+
+  changeCryptoImage(selectedCrypto) {
+    for (const crypto of this.cryptoList) {
+     if (selectedCrypto === crypto.ticker) {
+      this.image.nativeElement.src = crypto.imageUrl;
+     }
+    }
+  }
 }
