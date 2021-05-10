266f786d7fe696440542bf32b7db523f49008f2a
Vladyslav Shchetyna <vshchetyna@lenovo.com>
2021-05-10 14:37:06 +0300

    1.5 [core] Angular Update version

--- client/src/app/components/modal/request-modal/request-modal.component.spec.ts
+++ client/src/app/components/modal/request-modal/request-modal.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RequestModalComponent } from './request-modal.component';
 
@@ -6,7 +6,7 @@ describe('RequestModalComponent', () => {
   let component: RequestModalComponent;
   let fixture: ComponentFixture<RequestModalComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RequestModalComponent ]
     })
--- client/src/app/shared/select-lang/select-lang/select-lang.component.spec.ts
+++ client/src/app/shared/select-lang/select-lang/select-lang.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SelectLangComponent } from './select-lang.component';
 
@@ -6,7 +6,7 @@ describe('SelectLangComponent', () => {
   let component: SelectLangComponent;
   let fixture: ComponentFixture<SelectLangComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SelectLangComponent ]
     })
